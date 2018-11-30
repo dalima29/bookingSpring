d81bb9944e98021a0b94866a399321abfb203227
dalima29 <dabol2911@gmail.com>
2018-11-30 17:55:58 +0100

    Inizio parte di login

--- src/main/java/it/ariadne/bookingspring/controller/MainController.java
+++ src/main/java/it/ariadne/bookingspring/controller/MainController.java
@@ -1,9 +1,11 @@
 package it.ariadne.bookingspring.controller;
 
+import java.security.Principal;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import it.ariadne.bookingspring.dao.RisorsaDAO;
 import it.ariadne.bookingspring.entity.Macchina;
 import it.ariadne.bookingspring.utils.TableResponse;
+import it.ariadne.bookingspring.utils.WebUtils;
 
 @Controller
 public class MainController {
@@ -21,6 +24,37 @@ public class MainController {
 	RisorsaDAO risorsaDAO;
 	@Autowired
 	TableResponse tableResponse;
+	
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(Model model) {
+ 
+        return "loginPage";
+    }
+ 
+    @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
+    public String logoutSuccessfulPage(Model model) {
+        model.addAttribute("title", "Logout");
+        return "logoutSuccessfulPage";
+    }
+    
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public String accessDenied(Model model, Principal principal) {
+ 
+        if (principal != null) {
+            User loginedUser = (User) ((Authentication) principal).getPrincipal();
+ 
+            String userInfo = WebUtils.toString(loginedUser);
+ 
+            model.addAttribute("userInfo", userInfo);
+ 
+            String message = "Hi " + principal.getName() //
+                    + "<br> You do not have permission to access this page!";
+            model.addAttribute("message", message);
+ 
+        }
+ 
+        return "403Page";
+    }
  
     @RequestMapping(value = { "/home" }, method = RequestMethod.GET)
     public String homePage(Model model) {
